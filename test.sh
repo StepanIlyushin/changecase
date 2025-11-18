536382d0f68d171eb7312a680b7d82a07909e685
Stepan <st142263@student.spbu.ru>
2025-11-18 12:40:28 +0300

    fix: update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./changecase -L tests/test1.txt out.txt && (cmp out.txt tests/test1-L.txt || exit 1)
-./changecase -U tests/test1.txt out.txt && (cmp out.txt tests/test1-U.txt || exit 1)
-./changecase -G tests/test1.txt out.txt && (cmp out.txt tests/test1-G.txt || exit 1)
-./changecase -T tests/test1.txt out.txt && (cmp out.txt tests/test1-T.txt || exit 1)
+./changecase -L tests/test1.txt out.txt && (cmp out.txt test1-L.txt || exit 1)
+./changecase -U tests/test1.txt out.txt && (cmp out.txt test1-U.txt || exit 1)
+./changecase -G tests/test1.txt out.txt && (cmp out.txt test1-G.txt || exit 1)
+./changecase -T tests/test1.txt out.txt && (cmp out.txt test1-T.txt || exit 1)
